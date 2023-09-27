83bb8ed91cd40882d7aaad3d5f9ab69df68852f6
Kevin Ponce <kib.ponce@gmail.com>
2023-09-26 20:43:31 -0400

    added page layout, header, footer

--- src/pages/layout/Footer.tsx
+++ src/pages/layout/Footer.tsx
@@ -1,5 +1,23 @@
+import styled from "styled-components";
+import { widths } from "../../utils/styles";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <StyledFooter>
+      <h2>AniWiki</h2>
+      <h5>demo app</h5>
+    </StyledFooter>
+  );
 };
+
+const StyledFooter = styled.div`
+  display: flex;
+  min-height: 150px;
+  background: #2b2d42;
+  color: #bdc1ff;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+`;
 
 export default Footer;
--- src/pages/layout/Header.tsx
+++ src/pages/layout/Header.tsx
@@ -1,5 +1,34 @@
+import styled from "styled-components";
+import { widths } from "../../utils/styles";
+
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <StyledHeader>
+      <Container>
+        <HeaderBar>
+          <h1>AniWiki</h1>
+        </HeaderBar>
+      </Container>
+    </StyledHeader>
+  );
 };
+
+const StyledHeader = styled.div`
+  display: flex;
+  min-height: 75px;
+  background: #2b2d42;
+  color: #bdc1ff;
+`;
+
+const Container = styled.div`
+  width: ${widths.regularPageWidth}px;
+  margin: 0 auto;
+`;
+
+const HeaderBar = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100%;
+`;
 
 export default Header;
