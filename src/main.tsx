227f6efd66d13ff97d3bab13488c6100208e834d
Kevin Ponce <kib.ponce@gmail.com>
2023-09-28 10:17:21 -0400

    install graphql client

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 import Pages from "./pages";
 import GlobalStyles from "./utils/styles";
 
+const client = new ApolloClient({
+  uri: "https://graphql.anilist.co",
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
+    <ApolloProvider client={client}>
+      <GlobalStyles />
+      <Pages />
+    </ApolloProvider>
   </React.StrictMode>
 );
